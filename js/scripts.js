a5c21b0dd2b956a0a6886b1942a96db5ac771c2b
Maikol Piris <38299299+MaikolPiris@users.noreply.github.com>
2018-07-18 16:11:40 -0300

    18/7/2018

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
-	$(document).on("scroll",function(){
-		if(($(document).scrollTop())>55){
-
-		}
-	});
+  $(document).scroll("on",function(){
+    if ($(document).scrollTop()>75){
+      $(".navbar").css("opacity","0.7");
+      $(".navbar-brand").fadeOut("slow");
+    } else{
+      $(".navbar-brand").fadeIn("slow");
+      $(".navbar").css("opacity","1.0");
+    }
+  });
+  $('body').scrollspy({target: ".navbar-nav", offset: 0});   
+  $("#myNavbar a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+      }, 400, function(){
+         window.location.hash = hash;
+      });
+    }  
+  });
 });
