366d95cfee0fc6c7aa453d08cf44fc339fca443c
Maikol Piris <ferdy.piris@gmail.com>
2018-07-22 20:52:23 -0300

    22/07/2018

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,34 @@ $(document).ready(function(){
     }  
   });
 });
+
+function validateForm() {
+    var name =  document.getElementById('name').value;
+    if (name == "") {
+        document.getElementById('status').innerHTML = "Name no puede estar vacío";
+        return false;
+    }
+    var email =  document.getElementById('email').value;
+    if (email == "") {
+        document.getElementById('status').innerHTML = "Email no puede estar vacío";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.getElementById('status').innerHTML = "Formato de EMail no válido";
+            return false;
+        }
+    }
+    var subject =  document.getElementById('subject').value;
+    if (subject == "") {
+        document.getElementById('status').innerHTML = "Asunto no puede estar vacío";
+        return false;
+    }
+    var message =  document.getElementById('message').value;
+    if (message == "") {
+        document.getElementById('status').innerHTML = "Message no puede estar vacío";
+        return false;
+    }
+    document.getElementById('status').innerHTML = "Enviando...";
+    document.getElementById('contact-form').submit();
+}
