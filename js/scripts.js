c937ec8796f3f704de39aba51c8a4fe318182c10
Maikol Piris <ferdy.piris@gmail.com>
2018-08-16 06:14:49 -0300

    Final_Posta

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,64 @@
 $(document).ready(function(){
+  $("#description1").click(function(){
+    if ($("#description1Response").hasClass("hide")){  
+      $("#description1Response").slideDown(500);
+      $("#description1Response").removeClass("hide");
+      $("#description1 i").removeClass("fa-plus-circle");
+      $("#description1 i").addClass("fa-minus-circle");
+    }
+    else{
+      $("#description1Response").slideUp(500);
+      $("#description1Response").addClass("hide");
+      $("#description1 i").removeClass("fa-minus-circle");
+      $("#description1 i").addClass("fa-plus-circle");
+    }
+  });
+
+  $("#description2").click(function(){
+    if ($("#description2Response").hasClass("hide")){  
+      $("#description2Response").slideDown(500);
+      $("#description2Response").removeClass("hide");
+      $("#description2 i").removeClass("fa-plus-circle");
+      $("#description2 i").addClass("fa-minus-circle");
+    }
+    else{
+      $("#description2Response").slideUp(500);
+      $("#description2Response").addClass("hide");
+      $("#description2 i").removeClass("fa-minus-circle");
+      $("#description2 i").addClass("fa-plus-circle");
+    }
+  });
+
+  $("#description3").click(function(){
+    if ($("#description3Response").hasClass("hide")){  
+      $("#description3Response").slideDown(500);
+      $("#description3Response").removeClass("hide");
+      $("#description3 i").removeClass("fa-plus-circle");
+      $("#description3 i").addClass("fa-minus-circle");
+    }
+    else{
+      $("#description3Response").slideUp(500);
+      $("#description3Response").addClass("hide");
+      $("#description3 i").removeClass("fa-minus-circle");
+      $("#description3 i").addClass("fa-plus-circle");
+    }
+  });
+
+  $("#description4").click(function(){
+    if ($("#description4Response").hasClass("hide")){  
+      $("#description4Response").slideDown(500);
+      $("#description4Response").removeClass("hide");
+      $("#description4 i").removeClass("fa-plus-circle");
+      $("#description4 i").addClass("fa-minus-circle");
+    }
+    else{
+      $("#description4Response").slideUp(500);
+      $("#description4Response").addClass("hide");
+      $("#description4 i").removeClass("fa-minus-circle");
+      $("#description4 i").addClass("fa-plus-circle");
+    }
+  });
+
   //Scrollspy
   $(document).scroll("on",function(){
     if ($(document).scrollTop()>75){
